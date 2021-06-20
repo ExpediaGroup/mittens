fbe285f75bc01520fa1f933d4a144adf756d4e32
rdy25 <rendyreivaldy@gmail.com>
2021-06-21 03:43:08 +0700

    add unit test to internal/pkg/internal/utils.go and internal/pkg/probe/file.go

---
+++ internal/pkg/internal/utils_test.go
@@ -0,0 +1,23 @@
+package internal
+
+import (
+	"mittens/internal/pkg/probe"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestCreateTempFile(t *testing.T) {
+	var content string = "testfile"
+	var generatedFile string = CreateTempFile(content)
+
+	// check if the file generated is exist
+	var expected bool = true
+	fileCheckBool, err := probe.FileExists(generatedFile)
+	require.NoError(t, err)
+
+	// clean up test file
+	probe.DeleteFile(generatedFile)
+	assert.Equal(t, expected, fileCheckBool)
+}
