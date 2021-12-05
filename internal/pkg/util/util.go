116cb8705019144a2e2bc0e95ea8b99bd4583b90
josepowera <41049275+josepowera@users.noreply.github.com>
2021-12-05 21:04:35 +0000

    updated ToHeaders with InterpolatePlaceholders

--- internal/pkg/util/util.go
+++ internal/pkg/util/util.go
@@ -29,8 +29,8 @@ func ToHeaders(headersFlag []string) map[string]string {
 			log.Printf("cannot find ':' separator in supplied header %s", h)
 			headers[strings.TrimSpace(kv[0])] = ""
 			continue
-		}
-		headers[strings.TrimSpace(kv[0])] = strings.TrimSpace(kv[1])
+		}		
+		headers[strings.TrimSpace(kv[0])] = placeholders.InterpolatePlaceholders(strings.TrimSpace(kv[1]))
 	}
 	return headers
 }
